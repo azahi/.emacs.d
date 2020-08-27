a3bc0b4a91849bb97e3727371a4fa657f1030925
Azat Bahawi <azahi@teknik.io>
2020-08-27 16:50:44 +0300

    Add mu4e

--- init.el
+++ init.el
@@ -2136,5 +2136,106 @@
 
 (use-package ivy-pass)
 
+;;
+;;; mu4e.
+;;
+
+(defvar e-mu4e-backend 'mbsync)
+
+(use-package mu4e
+  :commands (mu4e mu4e-compose-new)
+  :init
+  (provide 'html2text) ;; Obsolete.
+  (when (or (not (require 'mu4e-meta nil t))
+            (version< mu4e-mu-version "1.4"))
+    (setq mu4e-maildir "~/.mail"
+          mu4e-user-mail-address-list nil))
+  (setq mu4e-attachment-dir
+        (lambda (&rest _)
+          (expand-file-name ".attachments" (mu4e-root-maildir))))
+  :config
+  (setq mu4e-get-mail-command "mbsync -a"
+        mu4e-change-filenames-when-moving t)
+
+  (setq mu4e-update-interval nil
+        mu4e-compose-format-flowed t
+        mu4e-view-show-addresses t
+        mu4e-sent-messages-behavior 'sent
+        mu4e-hide-index-messages t
+        mu4e-view-show-images t
+        mu4e-view-image-max-width 800
+        message-send-mail-function #'smtpmail-send-it
+        smtpmail-stream-type 'starttls
+        message-kill-buffer-on-exit t
+        mu4e-context-policy 'pick-first
+        mu4e-compose-context-policy 'ask-if-none
+        mu4e-completing-read-function #'ivy-completing-read
+        mu4e-confirm-quit nil
+        mu4e-headers-fields
+        '((:account . 12)
+          (:human-date . 12)
+          (:flags . 4)
+          (:from . 25)
+          (:subject)))
+
+  (setq mail-user-agent 'mu4e-user-agent)
+
+  ;; Add a column to display what email account the email belongs to.
+  (add-to-list 'mu4e-header-info-custom
+               '(:account
+                 :name "Account"
+                 :shortname "Account"
+                 :help "Which account this email belongs to"
+                 :function
+                 (lambda (msg)
+                   (let ((maildir (mu4e-message-field msg :maildir)))
+                     (format "%s" (substring maildir 1 (string-match-p "/" maildir 1)))))))
+
+  (advice-add #'mu4e-mark-execute-all :after
+    (lambda (&rest _)
+      (mu4e-headers-rerun-search)))
+
+  ;; Wrap text in messages.
+  (add-hook 'mu4e-view-mode-hook
+    (lambda (&rest _)
+      (setq truncate-lines nil)))
+
+  ;; HTML mails might be better rendered in a browser.
+  (add-to-list 'mu4e-view-actions '("View in browser" . mu4e-action-view-in-browser))
+
+  (when (fboundp 'imagemagick-register-types)
+    (imagemagick-register-types))
+
+  (defun set-email-account! (label letvars &optional default-p)
+    (with-eval-after-load 'mu4e
+      (when (version< mu4e-mu-version "1.4")
+        (when-let (address (cdr (assq 'user-mail-address letvars)))
+          (add-to-list 'mu4e-user-mail-address-list address)))
+      (setq mu4e-contexts
+            (cl-loop for context in mu4e-contexts
+                    unless (string= (mu4e-context-name context) label)
+                    collect context))
+      (let ((context (make-mu4e-context
+                      :name label
+                      :enter-func (lambda () (mu4e-message "Switched to %s" label))
+                      :leave-func #'mu4e-clear-caches
+                      :match-func
+                      (lambda (msg)
+                        (when msg
+                          (string-prefix-p (format "/%s" label)
+                                          (mu4e-message-field msg :maildir))))
+                      :vars letvars)))
+        (push context mu4e-contexts)
+        (when default-p
+          (setq-default mu4e-context-current context))
+        context))))
+
+(use-package org-msg
+  :hook (org-load . org-msg-mode)
+  :config
+  (setq org-msg-startup "inlineimages"
+        org-msg-greeting-name-limit 3
+        org-msg-text-plain-alternative t))
+
 (provide 'init)
 ;;; init.el ends here
